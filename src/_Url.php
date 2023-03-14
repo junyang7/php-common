7165b7ec7829561771a9413f71eb61e99eee9a1b
junyang7 <junyang7@staff.weibo.com>
2023-03-14 16:32:35 +0800

    修改：[_Url]修复port判断

--- src/_Url.php
+++ src/_Url.php
@@ -5,12 +5,12 @@ namespace Junyang7\PhpCommon;
 class _Url
 {
 
-    public static function get($protocol, $domain, $port = "", $uri = "", $parameter = [])
+    public static function get($protocol, $domain, $port = 0, $uri = "", $parameter = [])
     {
 
         $res = $protocol . "://" . $domain;
 
-        if (80 == $port && "http" != $protocol || 443 == $port && "https" != $protocol) {
+        if (!($port == 0 || 80 == $port && "http" == $protocol || 443 == $port && "https" == $protocol)) {
             $res .= ":" . $port;
         }
 
